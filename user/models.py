a39c69c015afc7e70eef58c41453a020135a980d
KimYeeun99 <kyz0608@naver.com>
2019-07-16 01:23:03 +0900

    delete

--- user/models.py
+++ user/models.py
@@ -8,7 +8,7 @@ class CustomerUser(AbstractUser,models.Model):
         ('female', 'Female'),
         )
     email = models.EmailField(unique=True,blank=False)
-    name = models.CharField(max_length=20,blank=False)
-    nickname = models.CharField(max_length=20,blank=False,unique=True)
-    gender = models.CharField(max_length=10,blank=False,choices=GENDER_CHOICES)
-    birth_date = models.DateField(null=False, blank=False)
+    name = models.CharField(max_length=20,blank=True)
+    nickname = models.CharField(max_length=20,blank=True,unique=True)
+    gender = models.CharField(max_length=10,blank=True,choices=GENDER_CHOICES)
+    birth_date = models.DateField(null=True, blank=True)
