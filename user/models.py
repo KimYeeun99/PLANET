f2c56f3101f417b8d1e927ddcb7a3ab323da8815
KimYeeun99 <kyz0608@naver.com>
2019-08-05 19:50:59 +0900

    commit

--- user/models.py
+++ user/models.py
@@ -7,8 +7,8 @@ class CustomerUser(AbstractUser):
         ('male', 'Male'),
         ('female', 'Female'),
         )
-    email = models.EmailField(unique=True,blank=False)
+    email = models.EmailField(unique=True,blank=True)
     name = models.CharField(max_length=20,blank=True)
     nickname = models.CharField(max_length=20,blank=True,unique=False)
     gender = models.CharField(max_length=10,blank=True,choices=GENDER_CHOICES)
-    birth_date = models.DateField(null=True, blank=False)
+    birth_date = models.DateField(null=True, blank=True)
